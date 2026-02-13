54ed4509de7715c5f4e0a74bcd709c883ad0ae70
creatic-2025 <bernaquezgirardd@educ.college-montreal.qc.ca>
2026-02-13 15:00:55 -0500

    init

--- attack_animation.py
+++ attack_animation.py
@@ -7,11 +7,8 @@ sprite_paper_attack = arcade.Sprite("fichier_images/spaper-attack.png", 1, 350, 
 sprite_scissors_idle = arcade.Sprite("fichier_images/scissors.png", 1, 500, 100)
 sprite_scissors_attack = arcade.Sprite("fichier_images/scissors-close.png", 1, 500, 100)
 
-attack_animations.append(sprite_rock_idle)
 attack_animations.append(sprite_rock_attack)
-attack_animations.append(sprite_paper_idle)
 attack_animations.append(sprite_paper_attack)
-attack_animations.append(sprite_scissors_idle)
 attack_animations.append(sprite_scissors_attack)
 
 
